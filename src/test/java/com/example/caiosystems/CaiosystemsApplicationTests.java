0adc990c2341adab9b9caefdd204aef8a1635c0d
Caio <caio.vr12@gmail.com>
2025-09-21 20:47:55 -0300

    estruturação da aplicação; testando primeiro endpoint

---
+++ src/test/java/com/example/caiosystems/CaiosystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caiosystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaiosystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
